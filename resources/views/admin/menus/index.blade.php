e651171f4f632dfc636e40379c308b6d7a05d213
DEE <donzydonathan@gmail.com>
2023-02-09 18:19:13 +0100

    feat: complete edit and delete actions for Table page

--- resources/views/admin/menus/index.blade.php
+++ resources/views/admin/menus/index.blade.php
@@ -42,7 +42,7 @@
                             </th>
                             <td class="px-6 py-4">
                                 <div class="flex space-x-2">
-                                    <a href="{{route('admin.menus.destroy', $menu->id)}}" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Edit</a>
+                                    <a href="{{route('admin.menus.edit', $menu->id)}}" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Edit</a>
                                     <form class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white" method="POST" action="{{route('admin.menus.destroy', $menu->id)}}" onsubmit="return confirm('Are you sure?')">
                                         @csrf
                                         @method('DELETE')
